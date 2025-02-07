e7b664f9c4b77bea0e977992f2f0b2930d06ed1c
Kristen De La Rosa <delarosa.kristen@gmail.com>
2025-02-07 01:08:25 -0800

    style: Favorite and Match page style update

--- src/components/DogCard/DogCard.component.tsx
+++ src/components/DogCard/DogCard.component.tsx
@@ -1,15 +1,9 @@
-import {
-  Badge,
-  Button,
-  Card,
-  Group,
-  Image,
-  Text,
-  DEFAULT_THEME as theme,
-} from "@mantine/core";
+import { Badge, Button, Card, Group, Image, Text } from "@mantine/core";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useLikedDogs } from "../../context/LikedDogsContext";
+import { theme } from "../../theme/theme";
 import { Dog } from "../../types";
+import classes from "./DogCard.module.css";
 
 interface IDogCardProps {
   dog: Dog;
@@ -22,32 +16,50 @@ export const DogCard: React.FunctionComponent<IDogCardProps> = (props) => {
 
   return (
     <Card
-      style={{ backgroundColor:  "var(--mantine-color-yellow-0)", maxWidth: "250px", minWidth: "200px" }}
+      style={{
+        backgroundColor: "var(--mantine-color-yellow-6)",
+      }}
+      w="300px"
+      h="350px"
       shadow="sm"
       padding="lg"
       radius="md"
       withBorder
     >
       <Card.Section>
-        <Image fit="contain" height={160} src={dog.img} alt={dog.name} />
+        <Image
+          mt="10px"
+          fit="contain"
+          height={160}
+          src={dog.img}
+          alt={dog.name}
+        />
       </Card.Section>
 
       <Group justify="space-between" mt="md" mb="xs">
         <Text fw={500}>{dog.name}</Text>
-        <Badge color={theme.colors?.yellow?.[6]}>{dog.breed}</Badge>
+        <Badge color={theme.colors?.yellow?.[0]}>{dog.breed}</Badge>
       </Group>
 
       <Group justify="space-between">
-        <Text size="sm" c="dimmed">
+        <Text size="sm" c="var(--mantine-colors-secondary-1)">
           Age: {dog.age}
         </Text>
-        <Text size="sm" c="dimmed">
+        <Text size="sm" c="var(--mantine-colors-secondary-1)">
           Zip: {dog.zip_code}
         </Text>
       </Group>
 
-      <Button variant="transparent" onClick={() => toggleLike(dog.id)}>
-        {isLiked ? <FaHeart color="red"/> : <FaRegHeart  color="red"/>}
+      <Button
+        className={classes.like}
+        variant="transparent"
+        onClick={() => toggleLike(dog.id)}
+      >
+        {isLiked ? (
+          <FaHeart color="var(--mantine-color-secondary-5)" />
+        ) : (
+          <FaRegHeart color="var(--mantine-color-secondary-5)" />
+        )}
       </Button>
     </Card>
   );
--- src/pages/Match/Match.page.tsx
+++ src/pages/Match/Match.page.tsx
@@ -1,18 +1,7 @@
-import {
-  AppShell,
-  Badge,
-  Card,
-  Group,
-  Image,
-  Stack,
-  Text,
-  DEFAULT_THEME as theme,
-  Title,
-} from "@mantine/core";
+import { Badge, Card, Group, Image, Stack, Text, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { Header } from "../../components/Header/Header.component";
-import { Navigation } from "../../components/Navigation/Navigation.component.";
 import { PageName } from "../../components/PageName/PageName.component";
+import { theme } from "../../theme/theme";
 import { Dog } from "../../types";
 import { fetchAllDogs, fetchBreeds, fetchDogsByIds } from "../../utils/api";
 
@@ -22,20 +11,15 @@ export const Match = () => {
 
   const getRandomFrom = (max: number = 1000): number => {
     const random = Math.floor(Math.random() * (max / 25)) * 25;
-    console.log("randomFrom", random);
     return random;
   };
 
   const getRandomBreed = async () => {
-    console.log("hello");
     const breedList = await fetchBreeds();
-    console.log("breedlist", breedList);
+
     if (breedList.length === 0) return undefined;
 
     const randomIndex = getRandomFrom(breedList.length);
-    console.log("randomIndex", randomIndex);
-    console.log(breedList[randomIndex]);
-
     return { breed: breedList[randomIndex], randomIndex };
   };
 
@@ -73,56 +57,52 @@ export const Match = () => {
   }, []);
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: "sm",
-      }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Header />
-      </AppShell.Header>
+    <Stack>
+      <PageName title="Find My Match" />
+      <Group justify="center">
+        {loading ? (
+          <p>Loading...</p>
+        ) : dog ? (
+          <Card
+            style={{
+              backgroundColor: "var(--mantine-color-yellow-6)",
+            }}
+            w="400px"
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+          >
+            <Card.Section>
+              <Image
+                w="375px"
+                radius={"lg"}
+                ml="10px"
+                mt="10px"
+                fit="contain"
+                src={dog.img}
+                alt={dog.name}
+              />
+            </Card.Section>
+            <Group justify="space-between" mt="md" mb="xs">
+              <Title order={3}>{dog.name}</Title>
 
-      <AppShell.Navbar>
-        <Navigation />
-      </AppShell.Navbar>
+              <Badge color={theme.colors?.yellow?.[0]}>{dog.breed}</Badge>
+            </Group>
 
-      <AppShell.Main>
-        <Stack>
-          <PageName title="Find My Match" />
-          <Group justify="center">
-            {loading ? (
-              <p>Loading...</p>
-            ) : dog ? (
-              <Card
-                style={{
-                  backgroundColor: "var(--mantine-color-yellow-0)",
-                  maxWidth: "250px",
-                  minWidth: "200px",
-                }}
-                shadow="sm"
-                padding="lg"
-                radius="md"
-                withBorder
-              >
-                <Card.Section>
-                  <Image fit="contain" src={dog.img} alt={dog.name} />
-                </Card.Section>
-                <Title order={3}>{dog.name}</Title>
-
-                <Badge color={theme.colors?.yellow?.[6]}>{dog.breed}</Badge>
-                <Text>Age: {dog.age} years</Text>
-
-                {/* <img src={dog.img} alt={dog.name} style={{ width: "100%" }} /> */}
-              </Card>
-            ) : (
-              <Text>No match yet, Textress the button to find a dog!</Text>
-            )}
-          </Group>
-        </Stack>
-      </AppShell.Main>
-    </AppShell>
+            <Group justify="space-between">
+              <Text size="sm" c="var(--mantine-colors-secondary-1)">
+                Age: {dog.age}
+              </Text>
+              <Text size="sm" c="var(--mantine-colors-secondary-1)">
+                Zip: {dog.zip_code}
+              </Text>
+            </Group>
+          </Card>
+        ) : (
+          <Text>No match yet, Textress the button to find a dog!</Text>
+        )}
+      </Group>
+    </Stack>
   );
 };
